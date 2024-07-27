159f5c87c98441be96adcd97586f1d0ddd4484f5
Uttam Kumar gain <138851080+uttam0611@users.noreply.github.com>
2024-07-27 12:24:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// const users = [
+//     {
+//         id: 1,
+//         name: "uttam",
+//         isActive: true,
+//         age: 22,
+//     },
+//     {
+//         id: 2,
+//         name: "vindhya",
+//         isActive: false,
+//         age: 22,
+//     },
+//     {
+//         id: 3,
+//         name: "suman",
+//         isActive: true,
+//         age: 22,
+//     },
+//     {
+//         id: 4,
+//         name: "sheetal",
+//         isActive: false,
+//         age: 22,
+//     },
+// ];
+
+// // printing all name from objects
+// const names = [];
+// // for(i=0; i<users.length; i++){
+// //     names.push(users[i].name);
+// // }
+// // console.log(names)
+
+// users.forEach((users)=>{
+//   names.push(users.name)
+// })
+// console.log(names)
+
+
